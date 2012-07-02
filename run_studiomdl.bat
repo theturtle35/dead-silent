e67c9eba27d2e95caea3ebfefa342e12c2e62249
theturtle35 <theturtle35@gmail.com>
2012-07-02 14:29:10 -0700

    Initial SDK, no changes.

---
+++ run_studiomdl.bat
@@ -0,0 +1,2 @@
+@"c:\program files (x86)\steam\steamapps\velociraptorpatrol\sourcesdk\bin\source2007\bin\studiomdl.exe" -game "c:\program files (x86)\steam\steamapps\SourceMods\deadsilent" %1 %2 %3 %4 %5 %6 %7 %8 %9
+
